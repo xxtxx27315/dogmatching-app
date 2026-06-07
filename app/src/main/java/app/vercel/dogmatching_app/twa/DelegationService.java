b70c0f4e903cbfac6f69e6048854a85f3e6d9496
xxtxx27315 <xx.t.xx273.15@gmail.com>
2026-06-07 21:36:59 +0900

    Add assetlinks.json for TWA

---
+++ app/src/main/java/app/vercel/dogmatching_app/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.dogmatching_app.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
